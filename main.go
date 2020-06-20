1d278e4c4423475edc3e97c618be7eb1f7981ec5
AlexSwiss <alexyikeh@gmail.com>
2020-06-20 09:06:42 +0100

    added function for getting a single class

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -9,6 +12,7 @@ import (
 
 // Our class model
 type class struct {
+	ID        string `json:"ID"`
 	Name      string `json:"name"`
 	StartDate string `json:"startdate"`
 	EndDate   string `json:"enddate"`
@@ -19,6 +23,7 @@ type allClasses []class
 
 var classes = allClasses{
 	{
+		ID:        "1",
 		Name:      "Pilate",
 		StartDate: "21 Jun 20",
 		EndDate:   "02 Dec 20",
@@ -34,14 +39,23 @@ func main() {
 	// API endpoints
 	router.HandleFunc("/classes", createClass)
 	router.HandleFunc("/allclasses", getClasses)
-	router.HandleFunc("/allclasses/{classID}", getClass)
+	router.HandleFunc("/allclasses/{id}", getClass)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 // create new classes
 func createClass(w http.ResponseWriter, r *http.Request) {
+	var newClass class
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintln(w, "Kindly enter values with ClassID, Name, StartDate, EndDate and Capacity only")
+	}
+	json.Unmarshal(reqBody, &newClass)
+	classes = append(classes, newClass)
+	w.WriteHeader(http.StatusCreated)
 
+	json.NewEncoder(w).Encode(newClass)
 }
 
 // Get all classes
@@ -51,5 +65,11 @@ func getClasses(w http.ResponseWriter, r *http.Request) {
 
 // Get single class
 func getClass(w http.ResponseWriter, r *http.Request) {
+	classID := mux.Vars(r)["id"]
 
+	for _, singleClass := range classes {
+		if singleClass.ID == classID {
+			json.NewEncoder(w).Encode(singleClass)
+		}
+	}
 }
